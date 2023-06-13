b91c4fd4f2f74cb8f761abd8054b05be1785195b
Alexis Reed <alexisritareed@gmail.com>
2023-06-13 15:49:37 -0700

    Progress

---
+++ src/js/processPage.js
@@ -0,0 +1,19 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper.min.css';
+import 'swiper/modules/pagination/pagination.min.css';
+import 'swiper/modules/navigation/navigation.min.css';
+
+const servicesswiper = new Swiper(document.getElementById('services-swiper'), {
+	loop: true,
+	modules: [Navigation, Pagination],
+	slidesPerView: 3,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		type: 'bullets',
+	},
+});
